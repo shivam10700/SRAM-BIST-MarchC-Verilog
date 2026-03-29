5425ba78284f99a23fa39aecbd44130220c16cbf
shivam10700 <104898091+shivam10700@users.noreply.github.com>
2026-03-30 02:11:25 +0530

    Update bist_top.v

--- tb/bist_top.v
+++ tb/bist_top.v
@@ -4,21 +4,25 @@ parameter ADDR_WIDTH = 4;
 parameter DATA_WIDTH = 8;
 
 reg clk = 0, rst = 1;
+
 wire [ADDR_WIDTH-1:0] addr;
 wire we;
-wire [DATA_WIDTH-1:0] din, dout;
 wire [1:0] state;
+wire phase;
 wire done;
+
+wire [DATA_WIDTH-1:0] din, dout;
 wire fail;
 
 always #5 clk = ~clk;
 
-bist_controller #(ADDR_WIDTH) ctrl(
+bist_controller ctrl(
     .clk(clk),
     .rst(rst),
     .addr(addr),
     .we(we),
     .state(state),
+    .phase(phase),
     .done(done)
 );
 
@@ -27,7 +31,7 @@ data_gen dg(
     .data(din)
 );
 
-sram #(ADDR_WIDTH, DATA_WIDTH) mem(
+sram mem(
     .clk(clk),
     .we(we),
     .addr(addr),
@@ -36,14 +40,22 @@ sram #(ADDR_WIDTH, DATA_WIDTH) mem(
 );
 
 comparator cmp(
-    .expected(din),
-    .actual(dout),
+    .clk(clk),
+    .we(we),
+    .din(din),   
+    .dout(dout),
     .fail(fail)
 );
+  
+// Dump waveform
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, bist_top);
+end
 
 initial begin
     #10 rst = 0;
-    #500 $finish;
+    #1000 $finish;
 end
 
 endmodule
