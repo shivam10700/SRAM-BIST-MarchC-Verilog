83f7b95b843bc6903e1e7973ebc2d9c60a5088b0
shivam10700 <104898091+shivam10700@users.noreply.github.com>
2026-03-30 02:11:01 +0530

    Update comparator.v

--- rtl/comparator.v
+++ rtl/comparator.v
@@ -1,14 +1,39 @@
+
 module comparator(
-    input [7:0] expected,
-    input [7:0] actual,
+    input clk,
+    input we,
+    input [7:0] din,
+    input [7:0] dout,
     output reg fail
 );
 
-always @(*) begin
-    if (expected != actual)
-        fail = 1;
-    else
-        fail = 0;
+reg [7:0] last_written;
+reg we_d;
+
+// Initialization
+initial begin
+    fail = 0;
+    last_written = 0;
+    we_d = 1;
+end
+
+// Track last written data
+always @(posedge clk) begin
+    if (we)
+        last_written <= din;
+end
+
+// Delay WE
+always @(posedge clk) begin
+    we_d <= we;
+end
+
+// Compare ONLY on read
+always @(posedge clk) begin
+    if (!we_d) begin
+        if (dout !== last_written)
+            fail <= 1;
+    end
 end
 
 endmodule
